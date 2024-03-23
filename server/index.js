9c5c3e96b4a343ac99a3981ab23a91c7b5fbb936
Nikhil241103 <pekacoclover@gmail.com>
2024-03-23 21:15:31 +0530

    configured index file

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express"
+import cors from "cors"
+import http from "http"
+import path, { dirname } from "path"
+import { fileURLToPath } from "url"
+
+const app = express()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+app.use(cors()) // need to configure for, only client side
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static("public"))
+app.use("/", express.static(path.join(__dirname, "../public/index.html")))
+
+const port = process.env.PORT || 5000
+
+app.use("/", (req, res) => {
+  res.send("This is server!")
+})
+
+const server = http.createServer(app)
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}...`)
+})

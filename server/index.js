4bb6c426c5980d7d396cde8b5deffa4fa90242a6
Nikhil241103 <pekacoclover@gmail.com>
2024-03-23 21:58:01 +0530

    added routes to index

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import http from "http"
 import path, { dirname } from "path"
 import { fileURLToPath } from "url"
 
+import routes from "./routes/index.routes.js"
+
 const app = express()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -13,6 +15,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static("public"))
 app.use("/", express.static(path.join(__dirname, "../public/index.html")))
+app.use("/api/v1", routes)
 
 const port = process.env.PORT || 5000
 

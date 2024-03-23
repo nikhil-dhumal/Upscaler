b42f2b421c707039a7881d01db6a59e94fded7e5
Nikhil241103 <pekacoclover@gmail.com>
2024-03-23 21:24:58 +0530

    configured multer middleware

---
+++ server/middlewares/multer.middleware.js
@@ -0,0 +1,42 @@
+import multer from "multer"
+import { diskStorage } from "multer"
+import fs from "fs/promises"
+import path from "path"
+import { fileURLToPath } from "url" 
+
+const storage = diskStorage({
+  destination: async (req, file, callback) => {
+    try {
+      const __dirname = path.dirname(fileURLToPath(import.meta.url))
+      const destDir = path.join(__dirname, "../input")
+
+      await fs.mkdir(destDir, { recursive: true })
+
+      callback(null, destDir)
+    } catch (error) {
+      callback(error, null)
+    }
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname)
+  },
+})
+
+const filter = (req, file, callback) => {
+  if (
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    callback(null, true)
+  } else {
+    callback(null, false)
+  }
+}
+
+const upload = multer({
+  storage: storage,
+  fileFilter: filter,
+})
+
+export default upload

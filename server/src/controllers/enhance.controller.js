6f094b3a9eeb0e3502152adcb6672158a0a24826
Nikhil241103 <pekacoclover@gmail.com>
2024-04-15 19:13:37 +0530

    redundant field filename removed

--- server/src/controllers/enhance.controller.js
+++ server/src/controllers/enhance.controller.js
@@ -7,6 +7,8 @@ const enhance = async (req, res) => {
     if (!req.file) return res.status(400).json({ message: "No image file provided" })
 
     const { file } = req
+    const splitArray = file.originalname.split(".")
+    const imgExt = splitArray[splitArray.length - 1]
     const imgData = file.buffer.toString("base64")
 
     const response = await axios.post(scoringURI, { image: imgData }, {
@@ -24,10 +26,9 @@ const enhance = async (req, res) => {
       const enhancedImgData = receivedData.image
 
       if (enhancedImgData) {
-        const filename = "enhaced_" + file.originalname
-        const imgUrl = `data:image/jpegbase64,${enhancedImgData}`
+        const imgUrl = `data:image/${imgExt};base64,${enhancedImgData.toString('base64')}`
 
-        return res.status(200).json({ filename, image: imgUrl })
+        return res.status(200).json({ image: imgUrl })
       } else {
         console.error("Error: No enhanced image data received from the server")
         return res.status(500).json({ error: "Error processing the image" })
@@ -36,8 +37,6 @@ const enhance = async (req, res) => {
       console.error("Error:", response.data)
       return res.status(500).json({ error: "Error processing the image" })
     }
-
-    return res.status(200).json({ data: response.data })
   } catch (error) {
     console.log(error)
     return res.status(500).json({

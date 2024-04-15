7581090471698743b3ee7861e87f8d184c45dd39
Nikhil241103 <pekacoclover@gmail.com>
2024-04-15 21:47:32 +0530

    modified heirarchy to be compatible for azure app service

--- server/src/routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import enhanceController from "../controllers/enhance.controller.js"
-import upload from "../middlewares/multer.middleware.js"
-
-const router = express.Router()
-
-router.post("/enhance", upload.single("image"), enhanceController.enhance)
-
-export default router
